9011ae02af0171e2094fd975080c1499363aad04
ROIM1998 <zhaobw1998@sina.com>
2023-02-27 02:52:15 +0000

    Fix main function bug in vibration sensor

--- example/VibraionSensor.py
+++ example/VibraionSensor.py
@@ -1,20 +1,21 @@
 import RPi.GPIO as GPIO
 
 class vibrationSensor():
-	def __init__(self,pinA,pinD):
-		self.pinA=pinA
+	def __init__(self,pinD=15):
+		#self.pinA=pinA
+		GPIO.setmode(GPIO.BOARD);
 		self.pinD=pinD
+		GPIO.setup(self.pinD, GPIO.IN)
+		
+	def read_value(self):
+		return GPIO.input(self.pinD)
 		
 		
 
 
 
 if __name__=="__main__":
-	
-	GPIO.setmode(GPIO.BOARD);
-	GPIO.setup(11, GPIO.IN)
-	GPIO.setup(13, GPIO.IN)
+	reader = vibrationSensor()
 	while True:
-		input_value = GPIO.input(11)
-		print("11pin: " + str(input_value),"13pin: "+str(GPIO.input(13)))
+		print("read value: %d" % reader.read_value())
 		
