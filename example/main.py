073df2c192cc04768676cacfd18a8650329bb090
ROIM1998 <zhaobw1998@sina.com>
2023-02-27 03:13:30 +0000

    Add multi-processing reader for vibration

--- example/main.py
+++ example/main.py
@@ -10,11 +10,14 @@ import numpy as np
 
 from threading import Thread, Lock
 from LuxSensor import readLight
+from VibraionSensor import vibrationSensor
 from DisplayControl import LCD
 sys.path.append("..")
 
 lux_history = []
 lux_lock = Lock()
+vibration_history = []
+vibration_lock = Lock()
 
 class LuxReader(Thread):
     def __init__(self, freq: float, limit: int = 10):
@@ -35,6 +38,28 @@ class LuxReader(Thread):
             lux_history.append(self.read_light())
             lux_lock.release()
             time.sleep(self.reading_delay)
+            
+class VibrationReader(Thread):
+    def __init__(self, freq: float, limit: int = 10):
+        super().__init__()
+        self.keep_reading = True
+        self.reading_freq = freq
+        self.reading_delay = 1 / freq
+        self.history_limit = limit
+        self.reader = vibrationSensor()
+        
+    def read_vibration(self):
+        return self.reader.read_value()
+    
+    def run(self):
+        while self.keep_reading:
+            vibration_lock.acquire()
+            if len(vibration_history) == self.history_limit:
+                vibration_history.pop(0)
+            vibration_history.append(self.read_vibration())
+            vibration_lock.release()
+            time.sleep(self.reading_delay)
+            
 
 if __name__=='__main__':
     # lcd=LCD(fps=60)
@@ -42,8 +67,11 @@ if __name__=='__main__':
     procs = []
     lux_reader = LuxReader(5)
     lux_reader.start()
+    vibration_reader = VibrationReader(5)
+    vibration_reader.start()
     while True:
-        print(lux_history)
+        print("Lux history read %.4f" % np.mean(lux_history))
+        print("Vibration history read %s" % np.array(vibration_history))
         time.sleep(1)
         
 
