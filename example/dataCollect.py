b1a3a782d9423d1e3cd6c86f6b89576a830d3592
sy <496825287@qq.com>
2023-03-16 18:06:32 -0700

    datacollection

--- example/dataCollect.py
+++ example/dataCollect.py
@@ -1,6 +1,7 @@
 import IMU
 import pickle
 from time import sleep
+import datetime
 
 def write_data(path,data):
     with open(path,'wb+') as f:
@@ -10,6 +11,12 @@ def load_data(path):
     with open(path,'rb+') as f:
         data=pickle.load(f)
     return data
+
+def get_time():
+    ISOTIMEFORMAT = '%d_%H_%M_%S'
+    theTime = datetime.datetime.now().strftime(ISOTIMEFORMAT)
+    return theTime
+
 
 def collect_data(path,sample_rate=10):
     imu = IMU.IMUsensor()
@@ -25,7 +32,7 @@ def collect_data(path,sample_rate=10):
 
 
 if __name__=="__main__":
-    PATH='/home/gix/TECHIN514/Shinmood/data.pkl'
+    PATH='/home/gix/TECHIN514/Shinmood/data_{}.pkl'.format(get_time())
     collect_data(PATH)
 
 
