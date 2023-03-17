c909f7bf13abd09aeef20c61f951684edec188ea
ROIM1998 <roimgame@gmail.com>
2023-03-17 13:13:15 +0800

    Fulfill pre_processing codes and training

--- example/gesture_ml.py
+++ example/gesture_ml.py
@@ -1,12 +1,33 @@
 import torch
 import pickle
+import numpy as np
 
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, Dataset
 from torch.nn import functional as F
 
-SAMPLE_RATE = 100
+SAMPLE_RATE = 10
 IN_FEATURES = 6
+TIME_WINDOW = 2
+
+normalize = {
+    'mean': {
+        'ax': -0.04649547031947544,
+        'ay': -0.7898887034824916,
+        'az': -0.1780448477608817,
+        'gx': -0.36303026172300984,
+        'gy': -0.03285850599781901,
+        'gz': -0.05809160305343511
+    },
+    'std': {
+        'ax': 0.2560270511848388,
+        'ay': 0.4629416712847358,
+        'az': 0.3843875035875543,
+        'gx': 6.684420876973939,
+        'gy': 19.601419629629802,
+        'gz': 6.819778407387476,
+    }
+}
 
 # Input feature would be a tensor with 6 features with the time of 1 second
 class GestureLSTMModule(torch.nn.Module):
@@ -17,7 +38,7 @@ class GestureLSTMModule(torch.nn.Module):
 
     def forward(self, x):
         x, _ = self.lstm(x)
-        x = self.fc(x)
+        x = self.fc(x[:, -1, :])
         x = F.sigmoid(x)
         return x
     
@@ -25,11 +46,11 @@ class GestureLSTMModule(torch.nn.Module):
 class GestureDNNModule(torch.nn.Module):
     def __init__(self):
         super(GestureDNNModule, self).__init__()
-        self.fc1 = torch.nn.Linear(IN_FEATURES * SAMPLE_RATE, 32)
+        self.fc1 = torch.nn.Linear(IN_FEATURES * SAMPLE_RATE * TIME_WINDOW, 32)
         self.fc2 = torch.nn.Linear(32, 1)
         
     def forward(self, x):
-        x = x.view(-1, IN_FEATURES * SAMPLE_RATE)
+        x = x.view(-1, IN_FEATURES * SAMPLE_RATE * TIME_WINDOW)
         x = F.gelu(self.fc1(x))
         x = F.sigmoid(self.fc2(x))
         return x
@@ -46,29 +67,44 @@ class GestureDataset(Dataset):
         return torch.Tensor(self.data[idx][0]).float().to(self.device), torch.Tensor([self.data[idx][1]]).float().to(self.device)
     
     
-def pre_process():
-    pass
+def pre_process(data):
+    data = data[200:-200]
+    ax, ay, az, gx, gy, gz = np.array([d[0][0] for d in data]), np.array([d[0][1] for d in data]), np.array([d[0][2] for d in data]), np.array([d[1][0] for d in data]), np.array([d[1][1] for d in data]), np.array([d[1][2] for d in data])
+    for d, name in zip([ax, ay, az, gx, gy, gz], ['ax', 'ay', 'az', 'gx', 'gy', 'gz']):
+        d -= normalize['mean'][name]
+        d /= normalize['std'][name]
+    returned_data = []
+    for i in range(0, len(data) - SAMPLE_RATE * TIME_WINDOW, SAMPLE_RATE):
+        returned_data.append(np.array([ax[i:i + SAMPLE_RATE * TIME_WINDOW], ay[i:i + SAMPLE_RATE * TIME_WINDOW], az[i:i + SAMPLE_RATE * TIME_WINDOW], gx[i:i + SAMPLE_RATE * TIME_WINDOW], gy[i:i + SAMPLE_RATE * TIME_WINDOW], gz[i:i + SAMPLE_RATE * TIME_WINDOW]]).T)
+    return returned_data
     
+
 if __name__ == '__main__':
     positive, negative = pickle.load(open('data/positive.pkl', 'rb')), pickle.load(open('data/negative.pkl', 'rb'))
+    positive, negative = pre_process(positive), pre_process(negative)
     dataset = [(x, 1) for x in positive] + [(x, 0) for x in negative]
     training, testing = train_test_split(dataset, test_size=0.2)
-    train_dataset, test_dataset = GestureDataset(training), GestureDataset(testing)
+    
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    train_dataset, test_dataset = GestureDataset(training, device), GestureDataset(testing, device)
     train_dataloader, test_dataloader = DataLoader(train_dataset, batch_size=32), DataLoader(test_dataset, batch_size=32)
     
-    model = GestureLSTMModule()
+    model = GestureDNNModule()
     for p in model.parameters():
         _ = p.requires_grad_(True)
     model.train()
+    model = model.to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+    criterion = torch.nn.BCELoss()
     accumulated_loss = 0
     training_steps = 0
-    num_epochs = 10
+    num_epochs = 100
     
+    best_f1 = 0
     for epoch in range(num_epochs):
         for x, y in train_dataloader:
             outputs = model(x)
-            loss = torch.nn.BCELoss(outputs, y)
+            loss = criterion(outputs, y)
             loss.backward()
             optimizer.step()
             model.zero_grad()
@@ -82,9 +118,16 @@ if __name__ == '__main__':
                     outputs = model(x)
                     predictions.extend(outputs)
                     labels.extend(y)
-                    eval_loss += torch.nn.BCELoss(outputs, y).item()
+                    eval_loss += criterion(outputs, y).item()
                 predictions = torch.cat(predictions, dim=0)
+                predictions, labels = (predictions > 0.5).cpu(), torch.Tensor(labels).int()
                 print(f'Eval loss: {eval_loss / len(test_dataloader)}')
-                print(f'Accuracy: {torch.sum((predictions > 0.5) == labels) / len(labels)}')
-                
-    torch.save(model.state_dict(), 'model.pt')
+                print(f'Accuracy: {torch.sum(predictions == labels) / len(labels)}')
+                # Calculating precision, recall, f1
+                precision = torch.sum(predictions * labels) / torch.sum(predictions)
+                recall = torch.sum(predictions * labels) / torch.sum(labels)
+                f1 = 2 * precision * recall / (precision + recall)
+                print(f'Precision: {precision}, Recall: {recall}, F1: {f1}')
+                if f1 > best_f1:
+                    best_f1 = f1
+                    torch.save(model.state_dict(), 'model.pt')
